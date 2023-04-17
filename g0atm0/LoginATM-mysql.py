ebbfd54bd01f641fb2746a109648b1c292cd8d67
czldl <602924803@qq.com>
2023-04-17 22:18:15 +0800

    较好的结构化设计v0.0.2

--- g0atm0/LoginATM-mysql.py
+++ g0atm0/LoginATM-mysql.py
@@ -8,6 +8,75 @@ import datetime
 '''
 pip3 install pymysql -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
 '''
+def isforbidden(item):
+    is_login = False
+    # 判断顾客是否被禁
+    if item["is_forbidden"]:
+        last_login_time = item["login_time"]
+        login_time = datetime.datetime.now()
+        waiting_time = int(((login_time - last_login_time).total_seconds()) / 60)
+        # 顾客如果被禁，判断还需要多长时间等待
+        if (waiting_time - 3) < 0:
+            print("账号暂时锁定，请等待%d分钟" % (3 - waiting_time))
+            #break
+            exit()
+def is3timespin(item):
+    times = 0
+    is_login = False
+    # 3次输入密码的机会
+    while times < 3:
+        password = input("请输入密码：")
+        if password != item["pin"]:
+            times += 1
+        else:
+            is_login = True
+            break
+    return is_login
+def islogin(is_login,userName):
+    # 登录成功与否，禁用信息以及登录信息数据都在数据库进行更新
+    if is_login:
+        is_forbidden = 0
+        # 将datetime转换字符串类型
+        login_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print("欢迎%s，登陆成功！" % (userName))
+    else:
+        is_forbidden = 1
+        login_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print("登录失败，请等待3分钟")
+    return is_forbidden,login_time
+def loginatm(connection):
+    while True:
+        userName = input("输入顾客名：")
+        if "q" != userName and "Q" != userName and len(userName)>2 and len(userName)<7:
+            with connection.cursor() as cursor:
+                # 根据顾客名查出顾客信息
+                sql = 'select * from t_customer where name = %s'
+                result = cursor.execute(sql, userName);
+                # 判断是否存在此顾客
+                if 0 == result:
+                    print("无此顾客名存在！")
+                    continue
+                # 获取顾客信息
+                item = cursor.fetchone()
+                # 判断是否被禁用
+                isforbidden(item);
+                # 判断是否超过3次输入密码
+                is_login=is3timespin(item);
+                # 判断是否登录成功
+                is_forbidden,login_time=islogin(is_login, userName);
+
+                # 更新数据库的Mysql语句
+                sql = "update t_customer set is_forbidden = %d , login_time = '%s' where name='%s'" % (
+                is_forbidden, login_time, userName)
+                cursor.execute(sql)
+
+                # 由于对数据库进行了更新，故需要提交事务
+                connection.commit()
+                break
+        else:
+            print("退出，顾客账号长度不在3-6之间！！！")
+            break
+
 #创建数据库的链接
 connection = pymysql.connect(host='127.0.0.1',
                              port=3306,
@@ -17,61 +86,7 @@ connection = pymysql.connect(host='127.0.0.1',
                              charset='utf8',
                              cursorclass=pymysql.cursors.DictCursor)
 try:
-    while True:
-        userName = input("输入顾客名：")
-        if "q"!=userName and "Q"!=userName:
-                with connection.cursor() as cursor:
-                    #根据顾客名查出顾客信息
-                    sql = 'select * from t_customer where name = %s'
-                    result =  cursor.execute(sql,userName);
-                    # 判断是否存在此顾客
-                    if 0==result:
-                        print("无此顾客名存在！")
-                        continue
-                    #获取顾客信息
-                    item = cursor.fetchone()
-                    is_login = False
-                    # 判断顾客是否被禁
-                    if item["is_forbidden"]:
-                        last_login_time = item["login_time"]
-                        login_time = datetime.datetime.now()
-                        waiting_time = int(((login_time-last_login_time).total_seconds())/60)
-                        #顾客如果被禁，判断还需要多长时间等待
-                        if (waiting_time-3)<0:
-                            print("账号暂时锁定，请等待%d分钟"%(3-waiting_time))
-                            break
-
-                    times = 0
-                    # 3次输入密码的机会
-                    while times<3:
-                        password = input("请输入密码：")
-                        if password != item["pin"]:
-                            times += 1
-                        else:
-                            is_login = True
-                            break
-
-                    # 登录成功与否，禁用信息以及登录信息数据都在数据库进行更新
-                    if is_login:
-                        is_forbidden = 0
-                        # 将datetime转换字符串类型
-                        login_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                        print("欢迎%s，登陆成功！"%(userName))
-                    else:
-                        is_forbidden = 1
-                        login_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                        print("登录失败，请等待10min")
-
-                    # 更新数据库的Mysql语句
-                    sql = "update t_customer set is_forbidden = %d , login_time = '%s' where name='%s'"%(is_forbidden,login_time,userName)
-                    cursor.execute(sql)
-
-                    # 由于对数据库进行了更新，故需要提交事务
-                    connection.commit()
-                    break
-        else:
-            print("退出！！！")
-            break
+    loginatm(connection);
 finally:
     # 最终关闭链接
     connection.close();
