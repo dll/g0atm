c45dc62a598c9924fc6b7d7a39cebb78d46bdcf8
czldl <602924803@qq.com>
2023-04-18 19:50:01 +0800

    测试文件中不该加模块g0atm0

--- g0atm0/test_login_atm_mysql.py
+++ g0atm0/test_login_atm_mysql.py
@@ -1,17 +1,17 @@
 import unittest
 from unittest.mock import Mock
-import g0atm0.login_atm_mysql
+import login_atm_mysql
 
 class Test_login_app(unittest.TestCase):
     def setUp(self):
-        g0atm0.login_atm_mysql.inputusername = Mock()
-        g0atm0.login_atm_mysql.inputusername.return_value = "atm"
-        g0atm0.login_atm_mysql.inputpin = Mock()
-        g0atm0.login_atm_mysql.inputpin.return_value = "gitops123"
+        login_atm_mysql.inputusername = Mock()
+        login_atm_mysql.inputusername.return_value = "atm"
+        login_atm_mysql.inputpin = Mock()
+        login_atm_mysql.inputpin.return_value = "gitops123"
 
     def test_createconnection(self):
-        self.assertIsNotNone(g0atm0.login_atm_mysql.createconnection())
+        self.assertIsNotNone(login_atm_mysql.createconnection())
 
     def test_islogin(self):
-        is_forbidden, login_time = g0atm0.login_atm_mysql.islogin(True, "atm")
+        is_forbidden, login_time = login_atm_mysql.islogin(True, "atm")
         self.assertEqual(is_forbidden, 0)
