02257c77ae766f765ac8b476572e3588a1592d00
ldl <ldl@chzu.edu.cn>
2023-05-04 07:29:06 +0800

    database vs dbname

--- g0atm0/test_dbutil.py
+++ g0atm0/test_dbutil.py
@@ -3,8 +3,6 @@ import unittest
 from unittest.mock import Mock
 from customer import Customer
 from dbutil import DbUtil
-from pgini import  PgIni
-
 
 class TestDbUtil(unittest.TestCase):
 
@@ -13,7 +11,6 @@ class TestDbUtil(unittest.TestCase):
         self.dbutil = DbUtil()
 
     def test_create_connection(self):
-        pgini = PgIni()
         self.assertIsNotNone(self.dbutil.create_connection(), "测试数据库链接，OK")
 
     def test_validate_id_true(self):
@@ -49,23 +46,23 @@ class TestDbUtil(unittest.TestCase):
     def test_validate_pin_true(self):
         DbUtil.input_pin = Mock()
         DbUtil.input_pin.return_value = "123456"
-        cpin = "123456";
+        cpin = "123456"
         self.assertEqual(self.dbutil.validate_pin(cpin, DbUtil.input_pin.return_value), True, "密码匹配")
 
     def test_validate_pin_false(self):
         DbUtil.input_pin = Mock()
         DbUtil.input_pin.return_value = "123456"
-        cpin = "222222";
+        cpin = "222222"
         self.assertEqual(self.dbutil.validate_pin(cpin, DbUtil.input_pin.return_value), False, "密码错误")
 
     def test_validate_pin_less6(self):
         DbUtil.input_pin = Mock()
         DbUtil.input_pin.return_value = "23456"
-        cpin = "222222";
+        cpin = "222222"
         self.assertEqual(self.dbutil.validate_pin(cpin, DbUtil.input_pin.return_value), False, "密码小于6位数字")
 
     def test_validate_pin_more6(self):
         DbUtil.input_pin = Mock()
         DbUtil.input_pin.return_value = "1234567"
-        cpin = "222222";
+        cpin = "222222"
         self.assertEqual(self.dbutil.validate_pin(cpin, DbUtil.input_pin.return_value), False, "密码大于6位数字")
