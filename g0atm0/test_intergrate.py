9d1508e6bf62fa67e92f230ef6d7922eb492fd2b
ldl <ldl@chzu.edu.cn>
2023-05-06 12:18:33 +0800

    实验二v0.0.211

--- g0atm0/test_intergrate.py
+++ g0atm0/test_intergrate.py
@@ -16,7 +16,30 @@ class TestIntergrate:
         suite.addTest(test_dbutil.TestDbUtil("test_validate_pin_true"))
         suite.addTest(test_customer.TestCustomer("test_login_true"))
 
-        # 运行测试用例
+        runner = unittest.TextTestRunner()
+        runner.run(suite)
+
+    def test_login_atm_id_fail(self):
+        # 将测试用例添加到测试集合
+        suite = unittest.TestSuite()
+        # 测试登录失败：账号错误
+        suite.addTest(test_dbutil.TestDbUtil("test_create_connection"))
+        suite.addTest(test_dbutil.TestDbUtil("test_validate_id_false"))
+        suite.addTest(test_customer.TestCustomer("test_login_false"))
+
+        runner = unittest.TextTestRunner()
+        runner.run(suite)
+
+    def test_login_atm_pin_fail(self):
+        # 将测试用例添加到测试集合
+        suite = unittest.TestSuite()
+        # 测试登录失败：密码错误
+        suite.addTest(test_dbutil.TestDbUtil("test_create_connection"))
+        suite.addTest(test_dbutil.TestDbUtil("test_validate_id_true"))
+        suite.addTest(test_dbutil.TestDbUtil("test_validate_pin_false"))
+        suite.addTest(test_customer.TestCustomer("test_login_false"))
+
+    # 运行测试用例
         runner = unittest.TextTestRunner()
         runner.run(suite)
 
